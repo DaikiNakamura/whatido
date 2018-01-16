d6dbe8f04d58fdab69ac19eb50d661641be74f6c
中村大騎 <d_nakamura@waku-2.com>
2018-01-16 14:59:56 +0900

    change taskList design
    table

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="taskList">
     <ul>
+      <!--表ヘッダー-->
+      <li class="task headerRow">
+        <span class="taskName">Task</span>
+        <span class="taskTime">Time</span>
+        <span class="taskDeleteButton">Delete</span>
+      </li>
+      <!--タスクたち-->
       <li class="task" v-for="(task, index) in tasks">
-        {{ task.taskName }}　({{ formatTaskTime(task.from) }} ～ {{ formatTaskTime(task.to) }})
-        <button v-on:click="deleteTask(index)" class="deleteButton" :style="{ backgroundColor: task.color }">DELETE</button>
+        <span class="taskName">{{ task.taskName }}</span>
+        <span class="taskTime">{{ formatTaskTime(task.from) }} ～ {{ formatTaskTime(task.to) }}</span>
+        <span class="taskDeleteButton"><button v-on:click="deleteTask(index)" class="deleteButton" :style="{ backgroundColor: task.color }">DELETE</button></span>
       </li>
     </ul>
   </div>
@@ -30,5 +38,47 @@
   }
   .task {
     font-size: 1.3em;
+  }
+
+  /* 表示形式 */
+  #taskList ul {
+    list-style: none;
+    display: table;
+  }
+  #taskList li {
+    display: table-row;
+  }
+  #taskList li span {
+    display: table-cell;
+  }
+  #taskList li span.taskName {
+    width: 300px;
+    word-break: break-all;
+  }
+  #taskList li span.taskTime {
+    width: 200px;
+  }
+  #taskList li span.taskDeleteButton {
+    width: 100px;
+  }
+  #taskList .headerRow span {
+    font-weight: bold;
+  }
+
+  /* 罫線 */
+  #taskList li span {
+    border-top: 1px solid #000;
+    border-left: 1px solid #000;
+  }
+  #taskList li span:last-child {
+    border-right: 1px solid #000;
+  }
+  #taskList li:last-child span {
+    border-bottom: 1px solid #000;
+  }
+
+  /* 動き */
+  #taskList li:not(.headerRow):hover {
+    background-color: #cccccc;
   }
 </style>
